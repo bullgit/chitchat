1edc9a0c58b61d49c5ebfcda926113987b27ad7c
Lucas Bonomi <lucas.bonomi@gmail.com>
2015-09-19 20:56:40 +0200

    Responsive and few improvements

--- src/public/scripts/main.js
+++ src/public/scripts/main.js
@@ -1,70 +1,101 @@
-var NONET = false;
+// Chitchat @bullgit
+// V0.1
 
-  var myFirebaseRef = new Firebase("https://bullchat.firebaseio.com/messages");
+// ====================================
+// Main.js
+// ====================================
 
-  var message_input =  document.querySelector('.messageinput');
-  var send_btn = document.querySelector('.sendbutton');
-  var message_container = document.querySelector('.chat-messages');
+var myFirebaseRef = new Firebase("https://bullchat.firebaseio.com/messages");
 
-  marked.setOptions({
-    highlight: function (code) {
-      return hljs.highlightAuto(code).value;
+var message_input =  document.querySelector('.messageinput');
+var send_btn = document.querySelector('.sendbutton');
+var message_container = document.querySelector('.chat-messages');
+
+// Init marked
+marked.setOptions({
+  highlight: function (code) {
+    return hljs.highlightAuto(code).value;
+  }
+});
+
+// ====================================
+// Push messages to firebase
+// ====================================
+
+// Detect when message is sent
+document.getElementById('send').addEventListener('click', function(){
+  
+  // Get the desired values for name / message
+  var username = document.getElementById('name').textContent;
+  var message = document.getElementById('message').textContent;
+
+  // If the message is empty don't do anything
+  if (message.length == 0) return false;
+
+  // If the message is not empty push it to firebase
+  myFirebaseRef.push({
+    id: "message-id-" +Math.random().toString(36).substr(2, 16)+"-msg",
+    data: {
+      author: username,
+      body: message
     }
   });
 
+  // when the message is sent scroll down the messages list
+  message_container.scrollTop = message_container.scrollHeight;
 
-    
-      document.getElementById('send').addEventListener('click', function(){
+  // Make sure to empty the reply-box
+  document.getElementById('message').innerHTML = "";
+});
 
-        var username = document.getElementById('name').textContent;
-        var message = document.getElementById('message').textContent;
-        if (message.length == 0) return false;
-        myFirebaseRef.push({
-          id: "message-id-" +Math.random().toString(36).substr(2, 16)+"-msg",
-          data: {
-            author: username,
-            body: message
-          }
-        });
-      message_container.scrollTop = message_container.scrollHeight;
-      document.getElementById('message').innerHTML = "";
-    });
-    
-    myFirebaseRef.on('child_added', function(dataSnapshot) {
-      var newPost = dataSnapshot.val();
-      var d = new Date();
-      var today = d.getMonth() + '-' + d.getDay() + '-' + d.getYear();
-      console.log(today);
-      console.log(newPost);
+// ====================================
+// Get messages from firebase
+// ====================================
+myFirebaseRef.on('child_added', function(dataSnapshot) {
+  var newPost = dataSnapshot.val();
+  var d = new Date();
+  var today = d.getMonth() + '-' + d.getDay() + '-' + d.getYear();
 
 
-       var template = '<message data-time-stamp="10-11-2015">' +
-      '<div class="user">' +
-          '  <span class="-avatar"> <img src="https://avatars.githubusercontent.com/' + newPost.data.author + '" alt="avatar user" /> </span>' +
-          '  <span class="-name"> '+ newPost.data.author +' </span>' +
-         ' </div>' +
-         ' <div class="-message">' +
-           ' <p>'+ marked(newPost.data.body) +'</p>' +
-          '</div>' +
-      '</message>';
+  // create message template
+  var template = '<message data-time-stamp="10-11-2015">' +
+  '<div class="user">' +
+  '  <span class="-avatar"> <img src="https://avatars.githubusercontent.com/' + newPost.data.author + '" alt="avatar user" /> </span>' +
+  '  <span class="-name"> '+ newPost.data.author +' </span>' +
+  ' </div>' +
+  ' <div class="-message">' +
+  ' <p>'+ marked(newPost.data.body) +'</p>' +
+  '</div>' +
+  '</message>';
+
+  // Generate message container
+  // And give it some attributes
+  var msg = document.createElement('div');
+  msg.classList.add('message-row');
+  msg.id = newPost.id; 
+
+  // Fill it with the template
+  msg.innerHTML = template;
+
+  // And add the message to the chat
+  document.querySelector('.chat-messages').appendChild(msg)
 
 
-      var msg = document.createElement('div');
-      msg.classList.add('message-row');
-      msg.id = newPost.id;
-      msg.innerHTML = template;
+  message_container.scrollTop = message_container.scrollHeight;
+});
 
-      document.querySelector('.chat-messages').appendChild(msg)
-      message_container.scrollTop = message_container.scrollHeight;
-    });
+// ====================================
+// MISC functions
+// ====================================
 
-    function searchKeyPress(e){
+// This function detect if the return key was pressed
+function searchKeyPress(e){
     // look for window.event in case event isn't passed in
     e = e || window.event;
     if (e.keyCode == 13)
     {
-        document.getElementById('send').click();
-        return false;
+      document.getElementById('send').click();
+      return false;
     }
     return true;
   }
