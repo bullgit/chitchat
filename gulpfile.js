57d0882a9ccebbee59510f1dc25a4a69422a5024
Lucas Bonomi <lucas.bonomi@tigerlilyapps.com>
2015-09-22 18:49:11 +0200

    Chaplin fixed

--- gulpfile.js
+++ gulpfile.js
@@ -73,13 +73,13 @@ gulp.task('chaplin', function () {
 gulp.task('connect', function () {
     var connect = require('connect');
     var app = connect()
-    .use(require('connect-livereload')({ port: 35729 }))
+    .use(require('connect-livereload')({ port: 3001 }))
     .use(connect.static('app'))
     .use(connect.static('.tmp'))
     .use(connect.directory('app'));
 
     require('http').createServer(app)
-    .listen(9292)
+    .listen(3000)
     .on('listening', function () {
       console.log('Started connect web server on http://localhost:9292');
     });
